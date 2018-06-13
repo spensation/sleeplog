6ecd7f1e37a5d7577779ffc2e1addc1c270d06b3
spensation <spensation@gmail.com>
2018-06-13 07:50:11 -0700

    migration and controllers done

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,2 +1,28 @@
 class DaysController < ApplicationController
+
+	def index
+		@days = Day.all
+	end
+
+	def new
+		@day = Day.new
+	end
+
+	def create
+		@day = Day.new(day_params)
+		if @day.save
+			redirect_to day_path(@day)
+		end
+	end
+
+	def show
+		@day = Day.find(params[:id])
+	end
+
+
+	private
+
+		def day_params
+			params.require(:day).permit(:name, :date)
+		end
 end
--- db/migrate/20180613124700_create_rests.rb
+++ db/migrate/20180613124700_create_rests.rb
@@ -2,7 +2,7 @@ class CreateRests < ActiveRecord::Migration[5.1]
   def change
     create_table :rests do |t|
       t.string :name
-      t.int :duration
+      t.integer :duration
 
       t.timestamps
     end
